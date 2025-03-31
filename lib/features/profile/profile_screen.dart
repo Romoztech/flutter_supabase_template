cde6480adbdf077123da7926ce77a8c572a84f44
Romoztech <hello@Romoz.tech>
2025-03-31 05:05:15 +0200

    Add ProfileScreen scaffold

---
+++ lib/features/profile/profile_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/flutter.dart';
+
+void class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBarKey: Key()
+      title: Text('Profile'),
+      body: Center(
+        child: Text('Profile Screen'),
+      ),
+    );
+  }
+}
