c0bdf8e8dfd6f0324118a84b356661888696d64a
Romoztech <hello@Romoz.tech>
2025-03-31 05:05:41 +0200

    Add CreatePostScreen scaffold

---
+++ lib/features/create_post/create_post_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/flutter.dart';
+
+void class CreatePostScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBarKey: Key()
+      title: Text('Create Post')
+      body: Center(
+        child: Text('Create Post Screen'),
+      ),
+    );
+  }
+}
