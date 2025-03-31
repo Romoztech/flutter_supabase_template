de98e9fc8fcae29d1d7b8b6cb14e23c0ca81666c
Romoztech <hello@Romoz.tech>
2025-03-31 05:19:13 +0200

    Add OurProjectsScreen widget

---
+++ lib/screens/our_projects.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/flutter.dart';
+
+class OurProjectsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBarKey: Key(),
+      title: Text('Our Projects'),
+      body: Center(
+        child: Text('Our projects screen'),
+      ),
+    );
+  }
+}
